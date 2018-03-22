dd6989d251a566e905c29fa5ff47857acaa686ef
Chris Holden <ceholden@gmail.com>
2018-03-22 09:40:56 -0400

    Be specific about lib/include dirs

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,11 @@
 
 mkdir build && cd build
 
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+cmake \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    -DCMAKE_INSTALL_INCLUDEDIR=include \
+    ..
 make
 make test
 make install
