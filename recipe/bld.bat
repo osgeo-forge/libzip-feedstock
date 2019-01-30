6f7b34451ee8c6bbf7643b69c1b4317cb023e4f5
Larry Shaffer <lshaffer@boundlessgeo.com>
2019-01-29 19:33:04 -0700

    Fix build type from debug to release

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,9 +5,11 @@ cd build
 if errorlevel 1 exit /B 1
 
 cmake -G "NMake Makefiles" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     ..
 if errorlevel 1 exit /B 1
 
-cmake --build . --target INSTALL --config Release
+cmake --build . --target INSTALL
 if errorlevel 1 exit /B 1
